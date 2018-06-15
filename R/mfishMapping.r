8c07192e37d505e9304f60c4b219464b12cbaebb
jeremymiller <jeremym@alleninstitute.org>
2018-06-15 16:40:27 -0700

    Update function plotHeatmap

--- R/mfishMapping.r
+++ R/mfishMapping.r
@@ -414,7 +414,7 @@ plotHeatmap <- function(datIn, group, groups = NULL, grouplab = "Grouping", useS
   # Make the plot!
   plotDat = rbind(plotDat,as.numeric(group)*cap/length(groups))
   plotDat = plotDat[,order(group, colSums(plotDat)) ]
-  rownames(plotDat) = c(colnames(plotDat)[1:(dim(plotDat)[1]-1)],grouplab)
+  rownames(plotDat) = c(rownames(plotDat)[1:(dim(plotDat)[1]-1)],grouplab)
   heatmap.2(plotDat, Rowv = Rowv, Colv = Colv, dendrogram = dendrogram, trace = trace, 
     margins = margins, rowsep = rowsep, key = key, col = colormap)
 }
